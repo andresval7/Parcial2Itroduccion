b84327a63ef280cc21947d86a5e4301bfc2deb47
andresval7 <andreolv@gmail.com>
2022-04-27 18:51:21 -0500

    Examen 2 Introduccion a la programacion

---
+++ login.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("login").addEventListener('submit', validarFormulario); 
+  });
+  
+  function validarFormulario(evento) {
+    evento.preventDefault();
+    var usuario = document.getElementById('usuario').value;
+    if(usuario.length == 0) {
+      alert('Favor ingresar el nombre del usuario');
+      return;
+    }
+    var clave = document.getElementById('contrasena').value;
+    if (clave =="12345") {
+      alert('La clave es inválida, favor ingrese una contrasena diferente');
+      return;
+    }
+    this.submit();
+  }
